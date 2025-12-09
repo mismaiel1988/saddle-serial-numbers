11c87db9e7cc4dff9b980471c60627a706125cf4
maya <mayamtrenk@gmail.com>
2025-12-09 15:41:10 -0700

    Fix AppProvider wrapping for Shopify embedded app

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,26 +1,29 @@
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
-import { Frame } from "@shopify/polaris";
+import {
+  AppProvider as ShopifyAppProvider,
+} from "@shopify/shopify-app-react-router/react";
+import { AppProvider as PolarisProvider, Frame } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
 
 import { Outlet, useLocation } from "react-router";
 
 export default function AppIndex() {
-  // Ensure we extract host from URL for App Bridge
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const host = params.get("host");
 
   return (
-    <AppProvider
+    <ShopifyAppProvider
       config={{
         apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
         host,
         forceRedirect: true,
       }}
     >
-      <Frame>
-        <Outlet />
-      </Frame>
-    </AppProvider>
+      <PolarisProvider i18n={enTranslations}>
+        <Frame>
+          <Outlet />
+        </Frame>
+      </PolarisProvider>
+    </ShopifyAppProvider>
   );
 }
