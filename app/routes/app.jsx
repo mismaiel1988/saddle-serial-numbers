3e2aa19fe72fa0e76ddef0ad67918195ee234002
maya <mayamtrenk@gmail.com>
2025-12-08 22:37:59 -0700

    Update app URLs, add env variables, fix AppProvider config

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,17 +1,14 @@
-// app/routes/app.jsx
-import {
-  AppProvider as PolarisAppProvider,
-  Frame
-} from "@shopify/polaris";
-
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
-import enTranslations from "@shopify/polaris/locales/en.json";
-
-import { Outlet } from "react-router";
+import { useAppBridge } from "@shopify/app-bridge-react";
 
 export default function AppLayout() {
   return (
-    <AppProvider>
+    <AppProvider
+      config={{
+        apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+        forceRedirect: true,
+      }}
+    >
       <PolarisAppProvider i18n={enTranslations}>
         <Frame>
           <Outlet />
