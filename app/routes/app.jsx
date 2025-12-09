22920519aedfdf07a4eb7c1d9dc8001fe6ce31b9
maya <mayamtrenk@gmail.com>
2025-12-08 20:41:43 -0700

    Downgrade app-bridge and simplify app wrapper

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,18 +1,11 @@
-import { Outlet, useLoaderData } from "react-router";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { Outlet } from "react-router";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
-  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+  return null;
 };
 
 export default function App() {
-  const { apiKey } = useLoaderData();
-
-  return (
-    <AppProvider isEmbeddedApp={true} apiKey={apiKey}>
-      <Outlet />
-    </AppProvider>
-  );
+  return <Outlet />;
 }
