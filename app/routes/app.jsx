f907307adce4f37d6cb8b075606942dbaafd60ce
maya <mayamtrenk@gmail.com>
2025-12-08 20:24:50 -0700

    Fix App Bridge initialization

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -11,7 +11,7 @@ export default function App() {
   const { apiKey } = useLoaderData();
 
   return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
+    <AppProvider isEmbeddedApp={true} apiKey={apiKey}>
       <Outlet />
     </AppProvider>
   );
